f8a3cf69885f245fdd96de2b9486b0020733303c
TQjona <wagner@tqsoft.de>
2020-07-22 10:25:27 +0200

    Allow overwriting FrameworkHistory

--- src/framework/History.ts
+++ src/framework/History.ts
@@ -1,7 +1,15 @@
-import { createBrowserHistory } from "history";
+import { createBrowserHistory, History } from "history";
 
 /**
  * The History used by the react-router instance provided by the framework
  * Can be used to navigate programmatically
  */
-export const FrameworkHistory = createBrowserHistory();
+export let FrameworkHistory = createBrowserHistory();
+
+/**
+ * The history can be overwritten for compatibility with other libraries
+ * @param history The history to use
+ */
+export const setFrameworkHistory = (history: History) => {
+	FrameworkHistory = history;
+};
