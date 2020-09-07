fbdb391303f6b9ebe17eba208dbb8f97a78bdd99
TQjona <wagner@tqsoft.de>
2020-09-07 11:47:51 +0200

    Framework: Export Theme Context

--- src/framework/index.tsx
+++ src/framework/index.tsx
@@ -1,6 +1,6 @@
 export { default as DialogContextProvider } from "./DialogContextProvider";
 export * from "./DialogContextProvider";
-
+export * from "./ThemeProvider";
 export * from "./History";
 
 export { default as Framework } from "./Framework";
