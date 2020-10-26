001949b4b2d1b1dfdc0c3dc9ef2e45b67bd394ff
TQjona <wagner@tqsoft.de>
2020-10-26 12:31:28 +0100

    Model: Add EnumMulti renderer "Checkbox"

--- src/backend-integration/Model/Types/Renderers/index.ts
+++ src/backend-integration/Model/Types/Renderers/index.ts
@@ -3,6 +3,7 @@ export { default as ModelDataTypeStringRendererMUI } from "./RendererString";
 export { default as ModelDataTypeEnumRadioRendererMUI } from "./RendererEnumRadio";
 export { default as ModelDataTypeEnumSelectRenderer } from "./RendererEnumSelect";
 export { default as ModelDataTypeEnumSelectRendererMUI } from "./RendererEnumSimpleSelect";
+export { default as ModelDataTypeEnumMultiCheckboxRendererMUI } from "./RendererEnumMultiCheckbox";
 export { default as ModelDataTypeDateNullableRendererMUI } from "./RendererDateNullable";
 export { default as ModelDataTypeDateRendererMUI } from "./RendererDate";
 export { default as ModelDataTypeBooleanCheckboxRendererMUI } from "./RendererBooleanCheckbox";
