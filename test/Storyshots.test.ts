85bc58fe1078667e9085be21f4a7cff6274799e9
TQjona <wagner@tqsoft.de>
2020-11-25 17:17:16 +0100

    Automatic testing with Storyshots

--- test/Storyshots.test.ts
+++ test/Storyshots.test.ts
@@ -1,6 +1,15 @@
 import "./js-dom-mocks";
 // import "./crud-mocks";
 import initStoryshots from "@storybook/addon-storyshots";
+import { advanceTo, clear } from "jest-date-mock";
+
+/**
+ * We have some controls which use the current Date (e.g. schedule controls).
+ * To prevent them from failing the snapshot because the date changed we're going to make it static
+ */
+
+beforeAll(() => advanceTo(1606219200000));
+afterAll(() => clear());
 
 initStoryshots({
 	storyKindRegex: /^(?!.*?(Backend-Components|Backend-Integration)).*$/gm,
