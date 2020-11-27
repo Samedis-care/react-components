7f19b635be88b49e1c72c554859a8dbdce71fe85
TQjona <wagner@tqsoft.de>
2020-11-27 17:35:09 +0100

    Add documentation

--- src/standalone/Notifications/index.tsx
+++ src/standalone/Notifications/index.tsx
@@ -17,20 +17,57 @@ import InfiniteScroll, { InfiniteScrollProps } from "../InfiniteScroll";
 import { makeStyles } from "@material-ui/core/styles";
 
 export interface Notification {
+	/**
+	 * An optional image for the notification (URL)
+	 */
 	image?: string;
+	/**
+	 * The main message of the notification
+	 */
 	message: React.ReactChild;
+	/**
+	 * The sub message of the notification, shown before it's age
+	 */
 	origin?: React.ReactChild;
+	/**
+	 * The timestamp of creation of this notification
+	 */
 	created: Date;
+	/**
+	 * When the notification expires (hides)
+	 */
 	expires?: Date;
+	/**
+	 * Has the user already read the notification?
+	 */
 	read: boolean;
 }
 
 export interface NotificationsProps {
+	/**
+	 * Properties to pass to the notification count badge
+	 */
 	BadgeProps?: BadgeProps;
+	/**
+	 * Properties to pass to the popover showing the notifications
+	 */
 	PopoverProps?: PopoverProps;
+	/**
+	 * Callback which is fired by the infinite scroll to load old notifications
+	 */
 	loadMore: InfiniteScrollProps["loadMoreBottom"];
+	/**
+	 * A custom notification renderer (optional)
+	 * @param notification The notification to render
+	 */
 	notificationRenderer?: (notification: Notification) => React.ReactElement;
+	/**
+	 * The notifications to display (should be updated by loadMore)
+	 */
 	notifications: Notification[];
+	/**
+	 * Event handler which fires when the user opens the notifications
+	 */
 	onOpen?: React.EventHandler<React.MouseEvent<HTMLButtonElement>>;
 }
 
