a7617b4495e6c3717166170ef627fa75da2a2885
neongrau <neongrau@gmail.com>
2021-01-05 12:21:14 +0100

    - code cleanups - size small support - examples for label formatting

--- src/standalone/UIKit/Checkbox.tsx
+++ src/standalone/UIKit/Checkbox.tsx
@@ -9,8 +9,9 @@ import {
 import { Theme } from "@material-ui/core/styles";
 
 const StyledCheckbox = withStyles((theme: Theme) => ({
-	root: () => ({
+	root: (props: CheckboxProps) => ({
 		"& span>svg": {
+			fontSize: props.size == "small" ? ".75em" : "1em",
 			borderWidth: "1px",
 			borderStyle: "solid",
 			borderColor: theme.palette.divider,
@@ -28,30 +29,30 @@ const StyledCheckbox = withStyles((theme: Theme) => ({
 	},
 }))(MuiCheckbox);
 
-const Checkbox = (props: CheckboxProps) => {
-	const _uncheckedIcon = (
-		<SvgIcon viewBox="-3.5 -4.5 24 24">
-			<polyline
-				id="check"
-				fill="transparent"
-				stroke="transparent"
-				strokeWidth="3.5"
-				points="1 7 6 12 16.5 1.5"
-			></polyline>
-		</SvgIcon>
-	);
-	const _checkedIcon = (
-		<SvgIcon viewBox="-3.5 -4.5 24 24">
-			<polyline
-				id="check"
-				fill="transparent"
-				stroke="currentColor"
-				strokeWidth="3.5"
-				points="1 7 6 12 16.5 1.5"
-			></polyline>
-		</SvgIcon>
-	);
+const _uncheckedIcon = (
+	<SvgIcon viewBox="-3.5 -4.5 24 24">
+		<polyline
+			id="check"
+			fill="transparent"
+			stroke="transparent"
+			strokeWidth="3.5"
+			points="1 7 6 12 16.5 1.5"
+		></polyline>
+	</SvgIcon>
+);
+const _checkedIcon = (
+	<SvgIcon viewBox="-3.5 -4.5 24 24">
+		<polyline
+			id="check"
+			fill="transparent"
+			stroke="currentColor"
+			strokeWidth="3.5"
+			points="1 7 6 12 16.5 1.5"
+		></polyline>
+	</SvgIcon>
+);
 
+const Checkbox = (props: CheckboxProps) => {
 	return (
 		<StyledCheckbox
 			color="primary"
--- src/stories/standalone/UIKit/Checkbox.story.tsx
+++ src/stories/standalone/UIKit/Checkbox.story.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { withActions } from "@storybook/addon-actions";
 import { boolean, text, withKnobs } from "@storybook/addon-knobs";
-
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import StyledCheckbox from "../../../standalone/UIKit/Checkbox";
+import { FormGroup, FormControlLabel, Typography } from "@material-ui/core";
 import Checkbox from "../../../standalone/UIKit/Checkbox";
 
 export const CheckboxStory = (): React.ReactElement => {
 	const [state, setState] = React.useState({
 		checkedA: true,
+		checkedB: false,
+		checkedC: false,
+		checkedD: true,
 	});
 
 	const handleChange = (event: {
@@ -19,19 +19,87 @@ export const CheckboxStory = (): React.ReactElement => {
 	};
 
 	return (
-		<FormGroup row>
-			<FormControlLabel
-				control={
-					<Checkbox
-						checked={state.checkedA}
-						disabled={boolean("Disabled", false)}
-						onChange={handleChange}
-						name="checkedA"
-					/>
-				}
-				label={text("Checkbox Label", "Checkbox Label")}
-			/>
-		</FormGroup>
+		<>
+			<FormGroup row>
+				<FormControlLabel
+					control={
+						<Checkbox
+							size="small"
+							checked={state.checkedA}
+							disabled={boolean("Disabled", false)}
+							onChange={handleChange}
+							name="checkedA"
+						/>
+					}
+					label={text("Checkbox A Label", "Checkbox A (size small)")}
+				/>
+				<FormControlLabel
+					labelPlacement="start"
+					control={
+						<Checkbox
+							size="medium"
+							checked={state.checkedB}
+							disabled={boolean("Disabled", false)}
+							onChange={handleChange}
+							name="checkedB"
+						/>
+					}
+					label={
+						<>
+							<Typography variant="caption" display="block" align="right">
+								Checkbox B
+							</Typography>
+							<Typography variant="caption" display="block" align="right">
+								Right (start) aligned label
+							</Typography>
+						</>
+					}
+				/>
+			</FormGroup>
+			<FormGroup row>
+				<FormControlLabel
+					labelPlacement="top"
+					control={
+						<Checkbox
+							size="small"
+							checked={state.checkedC}
+							disabled={boolean("Disabled", false)}
+							onChange={handleChange}
+							name="checkedC"
+						/>
+					}
+					label={text(
+						"Checkbox C Label",
+						"Checkbox C (size small) (top label)"
+					)}
+				/>
+				<FormControlLabel
+					labelPlacement="bottom"
+					control={
+						<Checkbox
+							size="medium"
+							checked={state.checkedD}
+							disabled={boolean("Disabled", false)}
+							onChange={handleChange}
+							name="checkedD"
+						/>
+					}
+					label={
+						<>
+							<Typography variant="caption" display="block" align="center">
+								Checkbox D
+							</Typography>
+							<Typography variant="caption" display="block" align="center">
+								Formatted
+							</Typography>
+							<Typography variant="caption" display="block" align="center">
+								Bottom Label
+							</Typography>
+						</>
+					}
+				/>
+			</FormGroup>
+		</>
 	);
 };
 
