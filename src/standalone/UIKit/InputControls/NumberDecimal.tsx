41170f9f3f67b141c4cf68e891de62bb0de0c4e4
ankitp <ankit.parekh@atharvasystem.com>
2021-01-06 14:54:57 +0530

    (Components-care/backlog/issues/12) WIP: Added number formating using globalize method

--- src/standalone/UIKit/InputControls/NumberDecimal.tsx
+++ src/standalone/UIKit/InputControls/NumberDecimal.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextFieldProps } from "@material-ui/core";
 import TextFieldWithHelp from "../TextFieldWithHelp";
 import { UIInputProps } from "../CommonStyles";
+import ccI18n from "../../../i18n";
+import Globalize from "globalize";
 
 export interface NumberDecimalProps extends UIInputProps {
 	infoText?: React.ReactNode;
@@ -10,14 +12,26 @@ export interface NumberDecimalProps extends UIInputProps {
 
 const NumberDecimal = (props: NumberDecimalProps & TextFieldProps) => {
 	const { infoText, important, ...muiProps } = props;
+	const [number, setNumber] = useState("");
+
+	const handleChange = (
+		event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+	) => {
+		const value = Number(event.target.value);
+		const newValue = Globalize.default(ccI18n.language).formatNumber(value);
+
+		setNumber(newValue);
+	};
 
 	return (
 		<div>
 			<TextFieldWithHelp
 				{...muiProps}
+				value={number}
 				infoText={infoText}
 				important={important}
 				type="number"
+				onChange={handleChange}
 			/>
 		</div>
 	);
