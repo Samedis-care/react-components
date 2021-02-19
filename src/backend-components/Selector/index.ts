64b107246bc5afc4c466fe9100d9459d243e1ee3
TQjona <wagner@tqsoft.de>
2021-02-19 17:39:44 +0100

    Add missing export

--- src/backend-components/Selector/index.ts
+++ src/backend-components/Selector/index.ts
@@ -1,3 +1,4 @@
 export { default as BackendMultiSelect } from "./BackendMultiSelect";
 export { default as BackendMultiSelectWithTags } from "./BackendMultiSelectWithTags";
 export { default as BackendSingleSelect } from "./BackendSingleSelect";
+export { default as CrudMultiSelect } from "./CrudMultiSelect";
