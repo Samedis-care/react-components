02026b012e713cbd7d6210faa0c851ff1dcece32
Dharmik <dharmikp@atharvasystem.com>
2021-02-24 19:05:15 +0530

    [CC-65] (https://github.com/Components-care/backlog/issues/65) Changes for date input using TextField

--- src/standalone/UIKit/InputControls/DateInput.tsx
+++ src/standalone/UIKit/InputControls/DateInput.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { IconButton, InputAdornment } from "@material-ui/core";
 import { DatePickerProps } from "@material-ui/pickers";
 import { Info as InfoIcon, Event as CalenderIcon } from "@material-ui/icons";
@@ -8,15 +8,32 @@ import {
 	useInputStyles,
 } from "../CommonStyles";
 import { LocalizedDatePicker } from "../../../standalone/LocalizedDateTimePickers";
-
+import TextFieldWithHelp from "../TextFieldWithHelp";
+import ccI18n from "../../../i18n";
+import moment from "moment";
+import { useInputCursorFix } from "../../../utils";
 export interface DateInputProps extends UIInputProps {
 	openInfo?: () => void;
 }
 
 const DateInput = (props: DateInputProps & DatePickerProps) => {
-	const { openInfo, important, ...muiProps } = props;
+	const {
+		value,
+		openInfo,
+		onChange,
+		important,
+		fullWidth,
+		placeholder,
+		...muiProps
+	} = props;
 	const inputClasses = useInputStyles({ important });
-
+	const [isOpen, setIsOpen] = useState(false);
+	const valueFormatted = value
+		? new Date(value as Date).toLocaleDateString(ccI18n.language)
+		: "";
+	const { handleCursorChange, cursorInputRef } = useInputCursorFix(
+		valueFormatted
+	);
 	const handleOpenInfo = useCallback(
 		(event: React.MouseEvent<HTMLButtonElement>) => {
 			// Prevent calender popup open event, while clicking on info icon
@@ -25,30 +42,58 @@ const DateInput = (props: DateInputProps & DatePickerProps) => {
 		},
 		[openInfo]
 	);
+	const handleTextFieldOnChange = useCallback(
+		(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+			handleCursorChange(event);
+
+			if (!onChange) return;
+			const momentDate = moment(event.target.value);
+			onChange(momentDate);
+		},
+		[onChange, handleCursorChange]
+	);
 
 	return (
-		<LocalizedDatePicker
-			{...muiProps}
-			clearable
-			InputProps={{
-				classes: inputClasses,
-				endAdornment: (
-					<InputAdornment position="end">
-						{!muiProps.disabled && (
-							<IconButton>
-								<CalenderIcon color={"disabled"} />
-							</IconButton>
-						)}
-						{openInfo && (
-							<IconButton onClick={handleOpenInfo}>
-								<InfoIcon color={"disabled"} />
-							</IconButton>
-						)}
-					</InputAdornment>
-				),
-			}}
-			InputLabelProps={InputLabelConfig}
-		/>
+		<>
+			<TextFieldWithHelp
+				fullWidth={fullWidth}
+				placeholder={placeholder}
+				type="text"
+				value={valueFormatted}
+				onChange={handleTextFieldOnChange}
+				InputProps={{
+					ref: cursorInputRef,
+					classes: inputClasses,
+					endAdornment: (
+						<InputAdornment position="end">
+							{!muiProps.disabled && (
+								<IconButton onClick={() => setIsOpen(true)}>
+									<CalenderIcon color={"disabled"} />
+								</IconButton>
+							)}
+							{openInfo && (
+								<IconButton onClick={handleOpenInfo}>
+									<InfoIcon color={"disabled"} />
+								</IconButton>
+							)}
+						</InputAdornment>
+					),
+				}}
+				InputLabelProps={InputLabelConfig}
+			/>
+
+			<LocalizedDatePicker
+				{...muiProps}
+				value={value}
+				onChange={onChange}
+				clearable
+				open={isOpen}
+				onOpen={() => setIsOpen(true)}
+				onClose={() => setIsOpen(false)}
+				TextFieldComponent={() => null}
+				InputLabelProps={InputLabelConfig}
+			/>
+		</>
 	);
 };
 
