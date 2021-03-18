2bb5f24db528a6c8cfe63a46b02d2586bcf1b03f
TQjona <wagner@tqsoft.de>
2021-03-18 10:24:00 +0100

    fileToData: Add option to include file name

--- src/utils/fileToData.ts
+++ src/utils/fileToData.ts
@@ -1,9 +1,22 @@
-const fileToData = async (file: File): Promise<string> => {
+/**
+ * Converts a file to a base64 data uri
+ * @param file The file to convert
+ * @param includeName Include the file name in the base64 data uri as name parameter?
+ */
+const fileToData = async (file: File, includeName = false): Promise<string> => {
 	// file -> data url
 	const reader = new FileReader();
 	return new Promise((resolve, reject) => {
 		reader.addEventListener("loadend", () => {
-			resolve(reader.result as string);
+			const dataUri = reader.result as string;
+			resolve(
+				includeName
+					? dataUri.replace(
+							";base64,",
+							`;name=${encodeURIComponent(file.name)};base64,`
+					  )
+					: dataUri
+			);
 		});
 		reader.addEventListener("error", () => {
 			reject(reader.error);
