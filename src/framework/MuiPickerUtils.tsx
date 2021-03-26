8dbce065566191b5f2e378bce1a6d0cc407c60d0
TQjona <wagner@tqsoft.de>
2021-03-26 16:12:05 +0100

    Fix moment issue

--- src/framework/MuiPickerUtils.tsx
+++ src/framework/MuiPickerUtils.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import MomentUtils from "@date-io/moment";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import ccI18n from "../i18n";
@@ -10,34 +10,8 @@ export interface MuiPickerUtilsProps {
 	children: React.ReactElement;
 }
 
-const updateLocale = () => {
-	void (async () => {
-		try {
-			await import("moment/locale/" + ccI18n.language.toLowerCase());
-		} catch (e) {
-			try {
-				await import(
-					"moment/locale/" + ccI18n.language.split("-")[0].toLowerCase()
-				);
-			} catch (e) {
-				// locale not found
-			}
-		} finally {
-			moment.locale(ccI18n.language);
-		}
-	})();
-};
-
 const MuiPickerUtils = (props: MuiPickerUtilsProps): React.ReactElement => {
 	const { disable } = props;
-
-	useEffect(() => {
-		if (disable) return;
-
-		updateLocale();
-		ccI18n.on("languageChanged", updateLocale);
-		return () => ccI18n.off("languageChanged", updateLocale);
-	}, [disable]);
 
 	if (disable) return props.children;
 
