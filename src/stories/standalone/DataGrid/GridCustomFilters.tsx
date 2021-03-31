99f37b5626dc813ae89741d30a23477c15fb4d6b
TQjona <wagner@tqsoft.de>
2021-03-31 11:12:13 +0200

    DataGrid: better default filter components

--- src/stories/standalone/DataGrid/GridCustomFilters.tsx
+++ src/stories/standalone/DataGrid/GridCustomFilters.tsx
@@ -1,85 +1,31 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { IDataGridFilterBarProps } from "../../../standalone/DataGrid/Header/FilterBar";
-import { Box, Grid, InputLabel } from "@material-ui/core";
-import { BaseSelectorData, SingleSelect } from "../../../standalone";
-
-interface CustomData {
-	filter1?: string;
-	filter2?: string;
-	filter3?: string;
-}
+import { Grid } from "@material-ui/core";
+import { GridSingleSelectFilter } from "../../../standalone";
 
 const options = ["Option 1", "Option 2", "Option 3"];
 
-const CustomFilterEntry = (
-	props: IDataGridFilterBarProps & { dataKey: keyof CustomData }
-) => {
-	const data = props.customData as CustomData;
-	const { setCustomData, dataKey } = props;
-
-	const setData = useCallback(
-		(value: string) => {
-			setCustomData((prev: CustomData) => ({
-				...prev,
-				[dataKey]: value,
-			}));
-		},
-		[setCustomData, dataKey]
-	);
-
-	return (
-		<>
-			{props.inDialog && <InputLabel>{dataKey}</InputLabel>}
-			<SingleSelect
-				autocompleteId={dataKey + (props.inDialog ? "-dialog" : "")}
-				disableSearch
-				onLoad={(query: string) =>
-					options
-						.filter((option) =>
-							option.toLowerCase().includes(query.toLowerCase())
-						)
-						.map(
-							(option): BaseSelectorData => ({
-								value: option,
-								label: option,
-							})
-						)
-				}
-				onSelect={(selected: BaseSelectorData | null) =>
-					setData(selected?.value || "")
-				}
-				selected={
-					data[dataKey]
-						? ({
-								value: data[dataKey],
-								label: data[dataKey],
-						  } as BaseSelectorData)
-						: null
-				}
-			/>
-		</>
-	);
-};
-
 const GridCustomFilters = (props: IDataGridFilterBarProps) => {
+	const { customData, setCustomData, inDialog } = props;
 	return (
-		<>
-			<Grid item xs>
-				<Box mt={1}>
-					<CustomFilterEntry {...props} dataKey={"filter1"} />
-				</Box>
-			</Grid>
-			<Grid item xs>
-				<Box mt={1}>
-					<CustomFilterEntry {...props} dataKey={"filter2"} />
-				</Box>
-			</Grid>
-			<Grid item xs>
-				<Box mt={1}>
-					<CustomFilterEntry {...props} dataKey={"filter3"} />
-				</Box>
-			</Grid>
-		</>
+		<Grid container>
+			{[1, 2, 3].map((filterId) => (
+				<GridSingleSelectFilter
+					key={filterId.toString()}
+					label={`Filter ${filterId}`}
+					options={options.map((option) => ({ value: option, label: option }))}
+					selected={(customData[`filter${filterId}`] as string) ?? ""}
+					onSelect={(selected) => {
+						setCustomData((old) => ({
+							...old,
+							[`filter${filterId}`]: selected,
+						}));
+					}}
+					dialog={inDialog}
+					autocompleteId={`filter${filterId}`}
+				/>
+			))}
+		</Grid>
 	);
 };
 
