ce2c6bc8c532103edc79cd2609be3b9236540c24
Vishesh Modi <vishesh.modi@atharvasystem.com>
2021-04-05 12:52:30 +0530

    [CC-81] Deleted Remove Icon as same available in material ui icons (named - Cancel) (https://github.com/Components-care/backlog/issues/81)

--- src/standalone/Icons/index.ts
+++ src/standalone/Icons/index.ts
@@ -1,5 +1,4 @@
 export { default as FilterIcon } from "./FilterIcon";
-export { default as RemoveIcon } from "./RemoveIcon";
 export { default as SignIcon } from "./SignIcon";
 export { default as TuneIcon } from "./TuneIcon";
 export { default as ResetIcon } from "./ResetIcon";
