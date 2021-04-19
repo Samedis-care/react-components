0d38d80449f15fbf3383c726595be55880036693
ankitp <ap@atharvasystem.com>
2021-04-19 11:39:00 +0530

    Hide calendar icon on disabled datepicker

--- src/standalone/LocalizedDateTimePickers/LocalizedKeyboardDatePicker.tsx
+++ src/standalone/LocalizedDateTimePickers/LocalizedKeyboardDatePicker.tsx
@@ -6,6 +6,7 @@ import {
 	KeyboardDatePicker,
 	KeyboardDatePickerProps,
 } from "@material-ui/pickers";
+import { Event as EventIcon } from "@material-ui/icons";
 
 type LocalizedKeyboardDatePickerProps = Omit<
 	KeyboardDatePickerProps,
@@ -53,6 +54,7 @@ const LocalizedKeyboardDatePicker = (
 			format={"L"}
 			refuse={/([^0-9./-])/gi}
 			rifmFormatter={TypeDate.format}
+			keyboardIcon={props.disabled ? null : <EventIcon />}
 			{...props}
 		/>
 	);
