c3fe0843166bb106f4b2c7c063a2d24fb8148b5d
TQjona <wagner@tqsoft.de>
2021-07-30 16:05:32 +0200

    [CC-94] Add clear button to TextFieldWithHelp on touch devices

--- src/standalone/UIKit/TextFieldWithHelp.tsx
+++ src/standalone/UIKit/TextFieldWithHelp.tsx
@@ -1,4 +1,10 @@
-import React, { ForwardedRef } from "react";
+import React, {
+	ForwardedRef,
+	useCallback,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 import {
 	TextField,
 	TextFieldProps,
@@ -6,8 +12,12 @@ import {
 	InputAdornment,
 	InputAdornmentProps,
 } from "@material-ui/core";
-import { Info as InfoIcon } from "@material-ui/icons";
+import {
+	Info as InfoIcon,
+	HighlightOff as ClearIcon,
+} from "@material-ui/icons";
 import { InputLabelConfig, UIInputProps, useInputStyles } from "./CommonStyles";
+import { isTouchDevice } from "../../utils";
 
 export interface TextFieldWithHelpProps extends UIInputProps {
 	/**
@@ -20,14 +30,59 @@ const TextFieldWithHelp = React.forwardRef(function TextFieldWithHelpInner(
 	props: TextFieldWithHelpProps & TextFieldProps,
 	ref: ForwardedRef<HTMLDivElement>
 ) {
-	const { openInfo, important, ...muiProps } = props;
+	const { openInfo, important, onChange, ...muiProps } = props;
 	const inputClasses = useInputStyles({ important });
+
+	// handle clear
+
+	const [hasValue, setHasValue] = useState<boolean>(
+		!!(muiProps.value ?? muiProps.defaultValue)
+	);
+
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	const handleClear = useCallback(() => {
+		if (!inputRef.current) {
+			throw new Error("InputRef not set");
+		}
+
+		const proto = window.HTMLInputElement.prototype;
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+			proto,
+			"value"
+		)?.set;
+		if (!nativeInputValueSetter) {
+			throw new Error("Native Input Value Setter is undefined");
+		}
+		nativeInputValueSetter.call(inputRef.current, "");
+
+		const event = new Event("input", { bubbles: true });
+		inputRef.current.dispatchEvent(event);
+	}, []);
+
+	// keep "hasValue" up to date
+
+	const handleChange = useCallback(
+		(evt: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+			if (onChange) onChange(evt);
+			setHasValue(!!evt.target.value);
+		},
+		[onChange]
+	);
+
+	useEffect(() => {
+		setHasValue(!!muiProps.value);
+	}, [muiProps.value]);
+
+	// render
 
 	return (
 		<TextField
 			ref={ref}
 			InputLabelProps={InputLabelConfig}
 			{...muiProps}
+			onChange={handleChange}
 			InputProps={{
 				classes: inputClasses,
 				...muiProps.InputProps,
@@ -39,6 +94,11 @@ const TextFieldWithHelp = React.forwardRef(function TextFieldWithHelpInner(
 									?.endAdornment as React.ReactElement<InputAdornmentProps>)
 									?.props?.children
 							}
+							{isTouchDevice() && hasValue && (
+								<IconButton onClick={handleClear}>
+									<ClearIcon />
+								</IconButton>
+							)}
 							<IconButton onClick={openInfo}>
 								<InfoIcon color={"disabled"} />
 							</IconButton>
@@ -47,6 +107,10 @@ const TextFieldWithHelp = React.forwardRef(function TextFieldWithHelpInner(
 				) : (
 					muiProps.InputProps?.endAdornment
 				),
+			}}
+			inputProps={{
+				...muiProps.inputProps,
+				ref: inputRef,
 			}}
 		/>
 	);
