095ccb77c0b3383e6394eb9341b2b4c59e640eac
TQjona <wagner@tqsoft.de>
2021-12-17 12:42:59 +0100

    BackendError: Extend for storing an error identifier

--- src/backend-integration/Connector/BackendError.ts
+++ src/backend-integration/Connector/BackendError.ts
@@ -1,7 +1,15 @@
 class BackendError extends Error {
-	constructor(msg: string) {
+	code: string | undefined;
+
+	/**
+	 * A error raised by backend
+	 * @param msg The message (human readable)
+	 * @param code Optional unique identifier for error
+	 */
+	constructor(msg: string, code?: string) {
 		super(msg);
 		this.name = "BackendError";
+		this.code = code;
 	}
 }
 
