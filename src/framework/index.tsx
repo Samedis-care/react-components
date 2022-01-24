636696ca6c38dfa2fb2a1d0580e3b83e74f0baed
TQjona <wagner@tqsoft.de>
2022-01-24 10:18:13 +0100

    Add missing re-export

--- src/framework/index.tsx
+++ src/framework/index.tsx
@@ -8,5 +8,6 @@ export * from "./CCI18nProvider";
 export { default as CCI18nProvider } from "./CCI18nProvider";
 export * from "./UnsafeToLeave";
 export { default as UnsafeToLeave } from "./UnsafeToLeave";
+export * from "./Storage";
 
 export { default as Framework } from "./Framework";
