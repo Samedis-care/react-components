132dffc190299c9199c2f5f7c38ea5bfd0e5df1f
TQjona <wagner@tqsoft.de>
2022-03-28 10:39:17 +0200

    Color Utils: Avoid using canvas

--- src/utils/combineColors.ts
+++ src/utils/combineColors.ts
@@ -4,24 +4,29 @@
  * @param color2 The second color (has to be transparent)
  * @returns A rgba array (usage for CSS: "rgba(" + combineColors(...).join() + ")")
  */
+import colorToRgba from "./colorToRgba";
+
 const combineColors = (
 	color1: string,
 	color2: string
-): [number, number, number, number] => {
-	// Jest patch, not used in prod
-	if (process && process.env && process.env.JEST_WORKER_ID) return [1, 1, 1, 1];
+): [r: number, g: number, b: number, a: number] => {
+	const color1Decoded = colorToRgba(color1);
+	const color2Decoded = colorToRgba(color2);
+	if (!color1Decoded || !color2Decoded)
+		throw new Error("Failed decoding colors");
 
-	const canvas = document.createElement("canvas");
-	const ctx = canvas.getContext("2d");
-	if (!ctx) throw new Error("Failed getting Canvas 2D context");
-	ctx.clearRect(0, 0, 1, 1);
-	ctx.fillStyle = color1;
-	ctx.fillRect(0, 0, 1, 1);
-	ctx.fillStyle = color2;
-	ctx.fillRect(0, 0, 1, 1);
-	const ret: number[] = [];
-	ctx.getImageData(0, 0, 1, 1).data.forEach((val) => ret.push(val));
-	return ret as [number, number, number, number];
+	const [r1, g1, b1, a1] = color1Decoded;
+	const [r2, g2, b2, a2] = color2Decoded;
+
+	if (a2 === 0) return color1Decoded; // 2nd color not visible, avoid division by zero when mixing
+
+	const mix: [r: number, g: number, b: number, a: number] = [0, 0, 0, 0];
+	const aR = (mix[3] = 1 - (1 - a2) * (1 - a1)); // alpha
+	mix[0] = Math.round((r2 * a2) / aR + (r1 * a1 * (1 - a2)) / aR); // red
+	mix[1] = Math.round((g2 * a2) / aR + (g1 * a1 * (1 - a2)) / aR); // green
+	mix[2] = Math.round((b2 * a2) / aR + (b1 * a1 * (1 - a2)) / aR); // blue
+
+	return mix;
 };
 
 export default combineColors;
