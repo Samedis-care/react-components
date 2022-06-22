bd72ab619ece22179cda8f5a854a24cf7ff94001
SCjona <jona@samedis.care>
2022-06-22 09:45:05 +0200

    SentryRoutes fix

--- src/standalone/SentryRoute/index.ts
+++ src/standalone/SentryRoute/index.ts
@@ -1,12 +1,18 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import * as Sentry from "@sentry/react";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const SentryRoute = Route; // TODO: Sentry.withSentryRouting(Route); does not work with React Router v6
+const SentryRoute = Route;
 
 export const SentryRoutes = Sentry
-	? Sentry.withSentryReactRouterV6Routing(Routes)
+	? // Sentry.withSentryReactRouterV6Routing will do nothing if Sentry has not been initialized yet, so we need to defer it using React.lazy
+	  React.lazy(() =>
+			Promise.resolve({
+				default: Sentry.withSentryReactRouterV6Routing(Routes),
+			})
+	  )
 	: Routes;
 
 export default (SentryRoute as unknown) as typeof Route;
