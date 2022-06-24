a7d8364a76a13d726508515619471c7230729814
SCjona <jona@samedis.care>
2022-06-24 14:41:36 +0200

    Sentry React Router v6 Instrumentation

--- src/standalone/SentryRoute/index.ts
+++ src/standalone/SentryRoute/index.ts
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import * as Sentry from "@sentry/react";
+import { withSentryReactRouterV6Routing } from "./reactRouterV6Instrumentation";
+export * from "./reactRouterV6Instrumentation";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -10,7 +12,7 @@ export const SentryRoutes = Sentry
 	? // Sentry.withSentryReactRouterV6Routing will do nothing if Sentry has not been initialized yet, so we need to defer it using React.lazy
 	  React.lazy(() =>
 			Promise.resolve({
-				default: Sentry.withSentryReactRouterV6Routing(Routes),
+				default: withSentryReactRouterV6Routing(Routes),
 			})
 	  )
 	: Routes;
