80df0af74a073b5e9579df855a3938d63154b724
SCjona <jona@samedis.care>
2022-07-27 16:16:35 +0200

    Fix action button hover style

--- src/standalone/UIKit/ActionButton.tsx
+++ src/standalone/UIKit/ActionButton.tsx
@@ -30,88 +30,93 @@ export interface ActionButtonTheme {
 	};
 }
 
-const StyledButton = withStyles((theme: Theme) => ({
-	root: (props: ActionButtonProps) => ({
-		border: theme.componentsCare?.uiKit?.actionButton?.border,
-		backgroundColor: props.color
-			? undefined
-			: props.backgroundColor ||
-			  theme.componentsCare?.uiKit?.actionButton?.backgroundColor ||
-			  theme.palette.primary.main,
-		color:
-			props.textColor ||
-			props.color ||
-			theme.componentsCare?.uiKit?.actionButton?.color ||
-			theme.palette.primary.contrastText,
-		fontSize: theme.componentsCare?.uiKit?.actionButton?.fontSize,
-		textTransform: "unset",
-		"&:hover": {
-			border:
-				theme.componentsCare?.uiKit?.actionButton?.hover?.border ||
-				theme.componentsCare?.uiKit?.actionButton?.border,
+const StyledButton = withStyles(
+	(theme: Theme) => ({
+		root: (props: ActionButtonProps) => ({
+			border: theme.componentsCare?.uiKit?.actionButton?.border,
 			backgroundColor: props.color
 				? undefined
-				: `rgba(${combineColors(
-						props.backgroundColor || theme.palette.primary.main,
-						theme.palette.action.hover
-				  ).join()})`,
-			...{
-				...theme.componentsCare?.uiKit?.actionButton?.style,
-				...theme.componentsCare?.uiKit?.actionButton?.hover?.style,
+				: props.backgroundColor ||
+				  theme.componentsCare?.uiKit?.actionButton?.backgroundColor ||
+				  theme.palette.primary.main,
+			color:
+				props.textColor ||
+				props.color ||
+				theme.componentsCare?.uiKit?.actionButton?.color ||
+				theme.palette.primary.contrastText,
+			fontSize: theme.componentsCare?.uiKit?.actionButton?.fontSize,
+			textTransform: "unset",
+			"&:hover": {
+				border:
+					theme.componentsCare?.uiKit?.actionButton?.hover?.border ||
+					theme.componentsCare?.uiKit?.actionButton?.border,
+				backgroundColor: props.color
+					? undefined
+					: `rgba(${combineColors(
+							props.backgroundColor ||
+								theme.componentsCare?.uiKit?.actionButton?.backgroundColor ||
+								theme.palette.primary.main,
+							theme.palette.action.hover
+					  ).join()})`,
+				...{
+					...theme.componentsCare?.uiKit?.actionButton?.style,
+					...theme.componentsCare?.uiKit?.actionButton?.hover?.style,
+				},
 			},
-		},
-		"&.Mui-disabled": {
-			backgroundColor: theme.palette.action.disabled,
-			...{
-				...theme.componentsCare?.uiKit?.actionButton?.style,
-				...theme.componentsCare?.uiKit?.actionButton?.disabled?.style,
+			"&.Mui-disabled": {
+				backgroundColor: theme.palette.action.disabled,
+				...{
+					...theme.componentsCare?.uiKit?.actionButton?.style,
+					...theme.componentsCare?.uiKit?.actionButton?.disabled?.style,
+				},
 			},
-		},
-		minWidth: props.small ? 0 : undefined,
-		padding: theme.componentsCare?.uiKit?.actionButton?.padding,
-		paddingLeft: props.small ? theme.spacing(3) : undefined,
-		paddingRight: props.small ? theme.spacing(3) : undefined,
-		...theme.componentsCare?.uiKit?.actionButton?.style,
+			minWidth: props.small ? 0 : undefined,
+			padding: theme.componentsCare?.uiKit?.actionButton?.padding,
+			paddingLeft: props.small ? theme.spacing(3) : undefined,
+			paddingRight: props.small ? theme.spacing(3) : undefined,
+			...theme.componentsCare?.uiKit?.actionButton?.style,
+		}),
+		startIcon: (props: ActionButtonProps) => ({
+			margin: props.small ? 0 : undefined,
+		}),
+		outlined: (props: ActionButtonProps) => ({
+			borderRadius:
+				theme.componentsCare?.uiKit?.actionButton?.borderRadius ||
+				theme.shape.borderRadius,
+			"&.Mui-disabled": {
+				border: theme.componentsCare?.uiKit?.actionButton?.border,
+				color: theme.palette.background.paper,
+				backgroundColor:
+					theme.componentsCare?.uiKit?.actionButton?.disabled?.backgroundColor,
+			},
+			padding: theme.componentsCare?.uiKit?.actionButton?.padding,
+			paddingLeft: props.small ? theme.spacing(3) : undefined,
+			paddingRight: props.small ? theme.spacing(3) : undefined,
+			...theme.componentsCare?.uiKit?.actionButton?.style,
+		}),
+		contained: (props: ActionButtonProps) => ({
+			borderRadius:
+				theme.componentsCare?.uiKit?.actionButton?.borderRadius ||
+				theme.shape.borderRadius,
+			"&.Mui-disabled": {
+				border: theme.componentsCare?.uiKit?.actionButton?.border,
+				color: theme.palette.background.paper,
+				backgroundColor:
+					theme.componentsCare?.uiKit?.actionButton?.disabled?.backgroundColor,
+			},
+			padding: theme.componentsCare?.uiKit?.actionButton?.padding,
+			paddingLeft: props.small ? theme.spacing(3) : undefined,
+			paddingRight: props.small ? theme.spacing(3) : undefined,
+			...theme.componentsCare?.uiKit?.actionButton?.style,
+		}),
+		label: (props: ActionButtonProps) => ({
+			padding: 0,
+			justifyContent: props.small ? "center" : "flex-start",
+			...theme.componentsCare?.uiKit?.actionButton?.label?.style,
+		}),
 	}),
-	startIcon: (props: ActionButtonProps) => ({
-		margin: props.small ? 0 : undefined,
-	}),
-	outlined: (props: ActionButtonProps) => ({
-		borderRadius:
-			theme.componentsCare?.uiKit?.actionButton?.borderRadius ||
-			theme.shape.borderRadius,
-		"&.Mui-disabled": {
-			border: theme.componentsCare?.uiKit?.actionButton?.border,
-			color: theme.palette.background.paper,
-			backgroundColor:
-				theme.componentsCare?.uiKit?.actionButton?.disabled?.backgroundColor,
-		},
-		padding: theme.componentsCare?.uiKit?.actionButton?.padding,
-		paddingLeft: props.small ? theme.spacing(3) : undefined,
-		paddingRight: props.small ? theme.spacing(3) : undefined,
-		...theme.componentsCare?.uiKit?.actionButton?.style,
-	}),
-	contained: (props: ActionButtonProps) => ({
-		borderRadius:
-			theme.componentsCare?.uiKit?.actionButton?.borderRadius ||
-			theme.shape.borderRadius,
-		"&.Mui-disabled": {
-			border: theme.componentsCare?.uiKit?.actionButton?.border,
-			color: theme.palette.background.paper,
-			backgroundColor:
-				theme.componentsCare?.uiKit?.actionButton?.disabled?.backgroundColor,
-		},
-		padding: theme.componentsCare?.uiKit?.actionButton?.padding,
-		paddingLeft: props.small ? theme.spacing(3) : undefined,
-		paddingRight: props.small ? theme.spacing(3) : undefined,
-		...theme.componentsCare?.uiKit?.actionButton?.style,
-	}),
-	label: (props: ActionButtonProps) => ({
-		padding: 0,
-		justifyContent: props.small ? "center" : "flex-start",
-		...theme.componentsCare?.uiKit?.actionButton?.label?.style,
-	}),
-}))(Button);
+	{ name: "CcActionButtonStyledButton" }
+)(Button);
 
 const StyledIconBox = withStyles(() => ({
 	root: {
