b2bdf8003684f233531842969237b05da1b15424
SCjona <jona@samedis.care>
2022-10-07 12:20:35 +0200

    dataToFile: include data uri in error if invalid

--- src/utils/dataToFile.ts
+++ src/utils/dataToFile.ts
@@ -5,7 +5,7 @@
  */
 const dataToFile = (data: string): Blob => {
 	const arr = data.split(",");
-	if (arr.length < 2) throw new Error("Invalid data uri");
+	if (arr.length < 2) throw new Error("Invalid data uri: " + data.slice(0, 64));
 	const mimeMatch = arr[0].match(/:(.*?);/);
 	if (!mimeMatch || mimeMatch.length < 2) throw new Error("mimeMatch invalid");
 	const mime = mimeMatch[1];
