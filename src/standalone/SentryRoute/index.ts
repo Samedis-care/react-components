a588615c5dabb9f9e5ec283547f73882d55034e5
SCjona <jona@samedis.care>
2023-03-13 13:52:11 +0100

    Remove custom react-router-v6 Sentry integration

--- src/standalone/SentryRoute/index.ts
+++ src/standalone/SentryRoute/index.ts
@@ -1,8 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import * as Sentry from "@sentry/react";
-import { withSentryReactRouterV6Routing } from "./reactRouterV6Instrumentation";
-export * from "./reactRouterV6Instrumentation";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -12,7 +10,7 @@ export const SentryRoutes = Sentry
 	? // Sentry.withSentryReactRouterV6Routing will do nothing if Sentry has not been initialized yet, so we need to defer it using React.lazy
 	  React.lazy(() =>
 			Promise.resolve({
-				default: withSentryReactRouterV6Routing(Routes),
+				default: Sentry.withSentryReactRouterV6Routing(Routes),
 			})
 	  )
 	: Routes;
