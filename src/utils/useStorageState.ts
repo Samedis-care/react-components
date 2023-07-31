c6c5ab307279bdcab855a967d5bd76eca92c3d9a
SCjona <jona@samedis.care>
2023-07-31 10:51:38 +0200

    useStorageState: Fix runtime error

--- src/utils/useStorageState.ts
+++ src/utils/useStorageState.ts
@@ -78,10 +78,13 @@ export const useLocalStorageState = <T>(
 				localStorage.setItem(storageKey, JSON.stringify(updatedValue));
 				return updatedValue;
 			});
-			stateUpdateListeners[storageKey].forEach((hook) => {
-				if (hook === setState) return;
-				hook(newValue);
-			});
+			if (storageKey in stateUpdateListeners) {
+				// storageKey may not be in stateUpdateListeners when the component is unmounted but a reference to this callback is still held
+				stateUpdateListeners[storageKey].forEach((hook) => {
+					if (hook === setState) return;
+					hook(newValue);
+				});
+			}
 		},
 		[storageKey]
 	);
