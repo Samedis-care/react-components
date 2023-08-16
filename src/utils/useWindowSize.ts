6077fdd35a847a43b113daf20242610bca3227ee
SCjona <jona@samedis.care>
2023-08-16 11:18:54 +0200

    useWindowSize util

---
+++ src/utils/useWindowSize.ts
@@ -0,0 +1,30 @@
+import { useEffect, useMemo, useState } from "react";
+import { debounce } from "./index";
+
+export type WindowSize = [width: number, height: number];
+
+const getWindowSize = (): WindowSize => {
+	return [window.innerWidth, window.innerHeight];
+};
+
+const useWindowSize = (): WindowSize => {
+	const [size, setSize] = useState<WindowSize>(getWindowSize);
+	const handleResize = useMemo(
+		() =>
+			debounce(() => {
+				setSize(getWindowSize);
+			}, 100),
+		[]
+	);
+
+	useEffect(() => {
+		addEventListener("resize", handleResize);
+		return () => {
+			removeEventListener("resize", handleResize);
+		};
+	}, [handleResize]);
+
+	return size;
+};
+
+export default useWindowSize;
