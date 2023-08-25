2d2416838b7097b4be53271c336e27c617a31555
SCjona <jona@samedis.care>
2023-08-25 12:02:01 +0200

    BackendError: Add custom meta

--- src/backend-integration/Connector/BackendError.ts
+++ src/backend-integration/Connector/BackendError.ts
@@ -1,15 +1,18 @@
 class BackendError extends Error {
 	code: string | undefined;
+	meta: unknown | undefined;
 
 	/**
 	 * A error raised by backend
 	 * @param msg The message (human readable)
 	 * @param code Optional unique identifier for error
+	 * @param meta Optional custom meta data
 	 */
-	constructor(msg: string, code?: string) {
+	constructor(msg: string, code?: string, meta?: unknown) {
 		super(msg);
 		this.name = "BackendError";
 		this.code = code;
+		this.meta = meta;
 	}
 }
 
