efbeabf1bbba5760a826935e8ad11ef3ff4d3fd8
SCjona <jona@samedis.care>
2024-01-11 17:10:26 +0100

    Model: Add generic type

---
+++ src/backend-integration/Model/Types/Renderers/RendererBackendType.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ModelRenderParams } from "../../../index";
+import Type from "../../Type";
+import FilterType from "../../FilterType";
+
+/**
+ * No-op renderer
+ */
+class RendererBackendType<T> implements Type<T> {
+	validate(): string | Promise<string | null> | null {
+		return null;
+	}
+	getFilterType(): FilterType {
+		return null;
+	}
+	getDefaultValue(): T {
+		return (null as unknown) as T;
+	}
+	stringify(value: T): string {
+		return JSON.stringify(value);
+	}
+
+	render(params: ModelRenderParams<T>): React.ReactElement {
+		const { visibility } = params;
+
+		if (visibility.disabled) return <></>;
+		throw new Error("Not supported");
+	}
+}
+
+export default RendererBackendType;
--- src/backend-integration/Model/Types/Renderers/index.ts
+++ src/backend-integration/Model/Types/Renderers/index.ts
@@ -13,3 +13,4 @@ export { default as ModelDataTypeBackendMultiSelectWithTagsRenderer } from "./Re
 export { default as ModelDataTypeBackendSingleSelectRenderer } from "./RendererBackendSingleSelect";
 export { default as ModelDataTypeStringArrayBackendRenderer } from "./RendererBackendStringArray";
 export { default as ModelDataTypeLocalizedStringRenderer } from "./RendererLocalizedString";
+export { default as ModelDataTypeAny } from "./RendererBackendType";
