3a1723633089d3e60be7dce9783a9c70e60c59c4
SCjona <jona@samedis.care>
2024-07-05 09:36:44 +0200

    useMountLogging: add ID

--- src/utils/useMountLogging.ts
+++ src/utils/useMountLogging.ts
@@ -1,16 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useId } from "react";
 
 /**
  * Debug utility for logging mounts/unmounts
  */
 const useMountLogging = (comp: { name: string }): void => {
 	const { name } = comp;
+	const id = useId();
 	useEffect(() => {
 		// eslint-disable-next-line no-console
-		console.log(name, "mounted");
+		console.log(name, "mounted", id);
 		return () => {
 			// eslint-disable-next-line no-console
-			console.log(name, "unmounted");
+			console.log(name, "unmounted", id);
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
