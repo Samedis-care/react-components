19e6082dead6c62d77efed14f6936ed153754430
SCjona <jona@samedis.care>
2025-02-28 17:13:06 +0100

    fix normalizeDate

--- src/backend-integration/Model/Types/Utils/DateUtils.ts
+++ src/backend-integration/Model/Types/Utils/DateUtils.ts
@@ -4,7 +4,9 @@
  * @returns A date "without time"
  */
 export const normalizeDate = (date: Date): Date => {
-	const newDate = new Date(date);
-	newDate.setUTCHours(12, 0, 0, 0); // set to noon, so when we read this from any timezone, including american timezones the date is correct
-	return newDate;
+	const year = date.getFullYear();
+	const month = date.getMonth();
+	const day = date.getDate();
+	// Create a new Date at 12:00 in UTC based on the local date parts.
+	return new Date(Date.UTC(year, month, day, 12, 0, 0));
 };
