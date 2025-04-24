e74fc5d28a57b4e63faa5e025fa2912256a47dad
SCjona <jona@samedis.care>
2025-04-24 11:15:13 +0200

    DefaultErrorComponent: Scroll to warning/error

--- src/backend-components/Form/DefaultErrorComponent.tsx
+++ src/backend-components/Form/DefaultErrorComponent.tsx
@@ -3,21 +3,34 @@ import { showErrorDialog } from "../../non-standalone";
 import { useDialogContext } from "../../framework";
 import { ErrorComponentProps } from "./Form";
 import { isValidationError } from "./ValidationError";
+import throwError from "../../utils/throwError";
 
 const DefaultErrorComponent = (props: ErrorComponentProps) => {
 	const propError = props.error;
 	const [pushDialog] = useDialogContext();
 
 	useEffect(() => {
+		let validationErrorClass: string | null = null;
 		if (isValidationError(propError)) {
 			// eslint-disable-next-line no-console
 			console.error("ValidationError", propError.mode, propError.result);
+			validationErrorClass =
+				propError.mode === "error"
+					? ".Mui-error"
+					: propError.mode === "warn"
+						? ".Mui-warning"
+						: throwError("not implemented");
 		} else {
 			// eslint-disable-next-line no-console
 			console.error(propError);
 		}
 
-		void showErrorDialog(pushDialog, propError);
+		void (async () => {
+			await showErrorDialog(pushDialog, propError);
+			if (!validationErrorClass) return;
+			const errElement = document.querySelector(validationErrorClass);
+			if (errElement) errElement.scrollIntoView();
+		})();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [propError]);
 
