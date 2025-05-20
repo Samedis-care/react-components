64926e936a431438730eb38df8446af88f85e77f
SCjona <jona@samedis.care>
2025-05-20 08:58:33 +0200

    Navigate: reset scroll position

--- src/standalone/Routes/useNavigate.ts
+++ src/standalone/Routes/useNavigate.ts
@@ -18,6 +18,11 @@ const useNavigate = (): NavigateFunction => {
 	const { history } = useHistoryRouterContext();
 	return useCallback(
 		(to: To, options?: NavigateOptions) => {
+			// reset scroll position
+			Array.from(document.querySelectorAll("*")).forEach((el) => {
+				el.scrollTop = 0;
+				el.scrollLeft = 0;
+			});
 			options ??= {};
 			if (options.replace) history.replace(to, options.state);
 			else history.push(to, options.state);
